9ebe5bf97ba834fd856f18729bf21886110fc3c5
EASR <53911966+schemell@users.noreply.github.com>
2025-02-03 20:43:16 +0100

    Add files via upload

---
+++ 01-variables.py
@@ -0,0 +1,16 @@
+"""System module."""
+variable = "Hola Mundo"
+numero = 42
+decimal = 10.5
+verdadero = True
+Falso = False
+
+nombre_curso = "Ultimate Python"
+nombre1 = "Ultimate Python"
+NOMBRE_CURSO = "Ultimate Python"
+Nombre_Curso = "Ultimate Python"
+print(nombre_curso, NOMBRE_CURSO, Nombre_Curso)
+
+alumnos = 5000
+puntaje = 9.9
+publicado = True
